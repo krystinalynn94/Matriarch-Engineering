f927fca9a5bf0c6c6d13f5f88f26153fcfb96120
Lunar Matriarch <krystina.moles@gmail.com>
2026-09-25 17:27:25 -0500

    Added home page with new hero location, added and routed About ME and Contact pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Portfolio } from './portfolio/portfolio';
+import {AboutME} from './about-me/about-me';
+import {Contact} from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', 
+    component: Home 
+  },
+  { path: 'portfolio', 
+    component: Portfolio 
+  },
+  { path: 'about-me',
+    component: AboutME
+  },
+  { path: 'contact',
+    component: Contact
+  }
+];
---
+++ src/app/portfolio/portfolio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-portfolio',
+  styleUrl: './portfolio.css',
+  templateUrl: './portfolio.html',
+})
+export class Portfolio {}
